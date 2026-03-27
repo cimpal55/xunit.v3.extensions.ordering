c1c84ce01c1ffe6316fbb877d0c33b5f2b6c91eb
cimpal55 <cimpals71@gmail.com>
2026-03-27 09:49:19 +0200

    Add project structure with test project

---
+++ src/xunit.v3.ordering/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace xunit.v3.extensions.ordering;
+
+public class Class1
+{
+
+}
---
+++ tests/xunit.v3.ordering.tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace xunit.v3.ordering.tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
